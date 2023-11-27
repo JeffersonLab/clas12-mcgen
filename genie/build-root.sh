0f9890eb2b73e7aea53a6f1fddfd1ac68938078e
Nathan Baltzell <baltzell@baltzellmac.jlab.org>
2023-11-27 11:03:54 -0500

    check existence of pythia library

--- genie/build-root.sh
+++ genie/build-root.sh
@@ -35,12 +35,16 @@ echo "SOURCE DIR:    $SOURCE_DIR"
 echo "BUILD DIR:     $BUILD_DIR"
 echo "INSTALL DIR:   $INSTALL_DIR"
 
+# Pythia library location (as created by build-deps.sh):
+PYTHIA_LIB=$(dirname $(realpath ${BASH_SOURCE[0]}))/lib/libPythia6.so
+
 # Check stuff and abort:
 [ $DRYRUN -ne 0 ] && exit
 [ -e $SOURCE_DIR ] && echo ERROR:  source directory already exists: root-$SOURCE_DIR.src && exit 1
 [ -e $BUILD_DIR ] && echo ERROR:  build directory already exists: $BUILD_DIR && exit 2
 [ -e $INSTALL_DIR ] && echo ERROR:  install directory already exists: $INSTALL_DIR && exit 3
-[ -e $LOG_FILE ] && echo ERROR:  log file already exists: $LOG_FILE && exit 4
+[ -e $PYTHIA_LIB ] && echo ERROR:  pythia6 library does not exist: $PYTHIA_LIB && exit 4
+[ -e $LOG_FILE ] && echo ERROR:  log file already exists: $LOG_FILE && exit 5
 
 # Echo all commands and abort if any return non-zero exit code:
 set -x -e
@@ -51,17 +55,11 @@ wget https://root.cern/download/root_v$ROOT_VERSION.source.tar.gz |& tee -a $LOG
 tar xzf root_v$ROOT_VERSION.source.tar.gz |& tee -a $LOG_FILE
 mv root-$ROOT_VERSION $SOURCE_DIR |& tee -a $LOG_FILE || exit 97
 
-# To find pythia (created by build-deps.sh):
-d=$(dirname $(realpath ${BASH_SOURCE[0]}))
-
 # Configure:
 cmake -S $SOURCE_DIR -B $BUILD_DIR \
  -DCMAKE_CXX_STANDARD=17 -DCMAKE_INSTALL_INSTALL_DIR=$INSTALL_DIR -Dbuiltin_glew=ON \
- -Dpythia6=ON -DPYTHIA6_LIBRARY=$d/lib/libPythia6.so \
+ -Dpythia6=ON -DPYTHIA6_LIBRARY=$PYTHIA_LIB \
  |& tee -a $LOG_FILE || exit 98
-
-# seems extrememly standard on linux distributions, no need to use local install:
-# -DGSL_DIR=$d
 
 # Build:
 cmake --build $BUILD_DIR --target install -- -j$NTHREADS |& tee -a $LOG_FILE || exit 99
